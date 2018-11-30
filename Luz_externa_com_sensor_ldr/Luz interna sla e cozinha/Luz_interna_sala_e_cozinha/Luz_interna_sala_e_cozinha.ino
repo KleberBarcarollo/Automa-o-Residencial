f305ff91a9b1f1253435d715ec7691c286b8a807
KleberBarcarollo <39749025+KleberBarcarollo@users.noreply.github.com>
2018-11-30 14:22:18 -0200

    Add files via upload

---
+++ Luz_externa_com_sensor_ldr/Luz interna sla e cozinha/Luz_interna_sala_e_cozinha/Luz_interna_sala_e_cozinha.ino
@@ -0,0 +1,26 @@
+/*
+* Projeto TCC (Trabalho conclusão de curso)
+* Automação residencial
+* Aluno Kleber Barcarollo
+* @autor Kleber Barcarollo
+* Curso Engenharia de Software
+* Instituição Unicesumar
+****************************************************
+* Nesta parte do projeto é a qual eu utilizo o esp 32
+* para ligar e desligar a luz da sala e da cozinha.
+*
+*/
+
+void setup()
+{
+  luzsala(5, OUTPUT);
+  luzcozinha(6, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(5, HIGH);
+  digitalWrite(5, LOW);
+  digitalWrite(6, HIGH);
+  digitalWrite(6, LOW);  
+}
---
+++ Luz_externa_com_sensor_ldr/Luz_externa_com_sensor_ldr_ino/Luz_externa_com_sensor_ldr_ino.ino
@@ -0,0 +1,24 @@
+/*
+* Projeto TCC (Trabalho conclusão de curso)
+* Automação residencial
+* Aluno Kleber Barcarollo
+* @autor Kleber Barcarollo
+* Curso Engenharia de Software
+* Instituição Unicesumar
+****************************************************
+* Nesta parte do projeto 
+*
+*/
+
+int sensorPin = A0; // Aqui o pino de entrada para LDR
+int sensorValue = 0; // variável para armazenar a luminosidade neste caso está em zero
+void setup() {
+Serial.begin(9600); // define porta serial para comunicação
+}
+
+void loop() {
+sensorValue = analogRead(sensorPin); // Aqui ele le o valor do sensor
+Serial.println(sensorValue); // imprime os valores de entrada a partir do sensor no Serial Mode
+delay(100); // Espera 
+}
+
